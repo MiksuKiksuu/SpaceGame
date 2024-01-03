8c4b4a4f4bca32a3fde4f6583900f7619d47f820
Miksu <37099589+MiksuKiksuu@users.noreply.github.com>
2024-01-03 13:44:39 +0200

    Add game file

---
+++ main.lua
@@ -0,0 +1,8 @@
+display.setStatusBar( display.HiddenStatusBar )
+
+local composer = require( "composer" )
+composer.recycleOnSceneChange = true
+
+composer.gotoScene( "Scenes.game", { time=500, effect="crossFade" } )
+
+
